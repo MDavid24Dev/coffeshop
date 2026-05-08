b751fcb9f60570db8d488697020011abce85a7bd
Miguel Hernandez <migueljk308@gmail.com>
2026-05-08 16:23:45 -0500

    "recomposicion home page logica y muestra de pantalla"

--- app/page.js
+++ app/page.js
@@ -2,13 +2,25 @@
 import { useEffect } from "react";
 import { supabase } from "@/lib/supabase.js"; //trayendo variables de entorno
 
-
-// Creamos el cliente pasando las variables
-//const supabase = createClient();
-
 export default function Home() {
 
-  // 2. Función de prueba para la base de datos
+ //0️⃣ ejecuta esta funcion y almacena en productos 
+ const productos = useSupabase();
+  //luego retorna y sigue por el retorn del Home
+  
+  /* me muestra en pantalla */
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#3e2723' }}>CoffeShop ☕</h1>
+      <p>El error 500 debería haber desaparecido.</p>
+      <p>Mira la <b>consola del navegador (F12)</b> para confirmar la conexión.</p>
+    </div>
+  );
+}
+
+/* funcion logica de supabase db */
+function useSupabase(){
+   // 2. Función de prueba para la base de datos
   const checkSupabase = async () => {
     try {
       const { data, error } = await supabase.from('productos').select('*');
@@ -29,11 +41,4 @@ export default function Home() {
 
   }, []);
 
-  return (
-    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1 style={{ color: '#3e2723' }}>CoffeShop ☕</h1>
-      <p>El error 500 debería haber desaparecido.</p>
-      <p>Mira la <b>consola del navegador (F12)</b> para confirmar la conexión.</p>
-    </div>
-  );
 }
